ab85bba7616d98c1e8f2ec5a2e5714a9c456b4f7
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-09-12 22:18:54 +0300

    changed for requirement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0 as ai
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, ai.add(a, b)))
+if __name__ == "__main__":
+    import add_0 as ai
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, ai.add(a, b)))
