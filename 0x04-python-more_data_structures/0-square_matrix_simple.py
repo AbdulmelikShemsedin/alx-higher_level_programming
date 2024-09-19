e2105ada5f83c4880dc98eefbf6439aba4f3eb2a
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-09-19 03:22:30 +0300

    advanced

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = matrix[:][:]
-    idx = 0
-    for i in new:
-        indx = 0
-        for j in i:
-            new[idx][indx] = j * j
-            indx += 1
-        idx += 1
-    return new
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
