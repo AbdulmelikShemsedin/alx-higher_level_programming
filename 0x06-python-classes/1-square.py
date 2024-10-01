ff3de29c4bae092bc05157279f41a7ad93687a23
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-10-01 22:22:46 +0300

    changed code styling

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Define a square class"""
 
+
 class Square:
     """ rep... of a square class"""
 
     def __init__(self, size):
-        """ instantation with size for our obect
+        """ instantation with size for our object
         initialization
         """
 
