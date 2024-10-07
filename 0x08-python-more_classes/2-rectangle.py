72c18bd487a29d06a2f34f2c5113ad4b12c29f54
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-10-07 17:20:44 +0300

    task 0,1,2 check

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            print("width must be an integer")
+            raise TypeError
+        elif value < 0:
+            raise ValueError
+            print("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            print("height must be an integer")
+            raise TypeError
+        elif value < 0:
+            print("height must be >= 0")
+            raise ValueError
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
