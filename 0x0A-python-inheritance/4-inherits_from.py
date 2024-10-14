fa3744e69302fba01fb76c985332be7d2e122ccf
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-10-14 22:34:33 +0300

    check tasks

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" fucntion that checks instance"""
+
+
+def inherits_from(obj, a_class):
+    """ returns true for correct instance"""
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
