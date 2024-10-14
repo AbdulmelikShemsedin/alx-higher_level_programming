b7a0d423255fc522d06a6c802883077a75d44fb3
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-10-14 22:42:09 +0300

    corrected

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,3 +6,4 @@ def inherits_from(obj, a_class):
     """ returns true for correct instance"""
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
+    return False
